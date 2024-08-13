e2844839d51b956d9a8646e6572452b954ec9307
root <root@DESKTOP-ATFCIID>
2024-08-13 17:45:08 -0400

    cuadrando texto para movil

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,8 +2,8 @@ const HeroSection = () => {
   return (
     <section className="h-[60vh] lg:h-[80vh] mt-32 lg:mt-28 w-full text-vinarmYellow font-montserrat bg-hero lg:bg-center bg-right bg-cover bg-no-repeat ">
       <div className="flex lg:ml-10 mx-24 items-center justify-end lg:justify-start z-30">
-        <div className=" w-[90px] h-[600px] lg:h-[500px] flex items-center ">
-          <h1 className="font-montserrat font-extrabold text-4xl lg:text-5xl">
+        <div className=" w-[95px] h-[600px] lg:h-[500px] flex items-center ">
+          <h1 className="font-montserrat font-extrabold text-3xl lg:text-5xl">
             Pa‘pasarla brutal
           </h1>
         </div>
