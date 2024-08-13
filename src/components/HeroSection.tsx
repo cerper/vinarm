1448c202add477f5366f25ee1e8c5b2c85db52c2
root <root@DESKTOP-ATFCIID>
2024-08-13 16:48:28 -0400

    cuadrando texto para movil

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 const HeroSection = () => {
   return (
     <section className="h-[50vh] lg:h-[80vh] mt-32 lg:mt-28 w-full text-vinarmYellow font-montserrat bg-hero lg:bg-center bg-right bg-cover bg-no-repeat ">
-      <div className="flex ml-8 mx-4  items-center justify-end lg:justify-start z-30">
+      <div className="flex ml-8 mx-2  items-center justify-end lg:justify-start z-30">
         <div className="ml-28 w-[95px] h-[300px] lg:h-[500px] flex items-center ">
           <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
         </div>
